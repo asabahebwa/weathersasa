1fb6899968bbdbc8fcb274e68810ad34942c07a7
Livingstone Asabahebwa <ahebwa49@gmail.com>
2024-03-02 02:53:43 +0300

    reduce bundle size

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import "zingchart/es6";
 import ZingChart from "zingchart-react";
 
 import { getWeatherForecast } from "./services/forecast";
