cabb2533ab09c1f57fa4227e44184b90b9538967
Livingstone Asabahebwa <lasabahebwa@gmail.com>
2025-04-14 16:45:31 +0300

    add mobile menu

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faTwitter,
   faFacebook,
   faInstagram,
-  faGithub,
+  faThreads,
+  faXTwitter,
 } from "@fortawesome/free-brands-svg-icons";
 import {
   faUserCircle,
@@ -16,6 +16,14 @@ import "../styles/MenuBar.css";
 const MenuBar = () => {
   // State to manage the mobile menu open/close state
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  React.useEffect(() => {
+    function handleResize() {
+      setMobileMenuOpen(false);
+    }
+
+    window.addEventListener("resize", handleResize);
+  });
 
   const toggleMobileMenu = () => {
     setMobileMenuOpen(!mobileMenuOpen);
@@ -64,17 +72,12 @@ const MenuBar = () => {
               <li>
                 <a href="/visualizations">Visualizations</a>
               </li>
+              <li>
+                <a href="/contact">Contact</a>
+              </li>
             </ul>
           </nav>
           <div className="social-icons">
-            <a
-              href="https://twitter.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Twitter"
-            >
-              <FontAwesomeIcon icon={faTwitter} />
-            </a>
             <a
               href="https://facebook.com"
               target="_blank"
@@ -92,80 +95,132 @@ const MenuBar = () => {
               <FontAwesomeIcon icon={faInstagram} />
             </a>
             <a
-              href="https://github.com"
+              href="https://threads.net"
               target="_blank"
               rel="noopener noreferrer"
-              aria-label="GitHub"
+              aria-label="Threads"
             >
-              <FontAwesomeIcon icon={faGithub} />
+              <FontAwesomeIcon icon={faThreads} />
+            </a>
+            <a
+              href="https://x.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="xTwitter"
+            >
+              <FontAwesomeIcon icon={faXTwitter} />
             </a>
           </div>
           <div className="hamburger-menu-container" onClick={toggleMobileMenu}>
             <button className="hamburger-menu" aria-label="Toggle mobile menu">
-              <FontAwesomeIcon icon={mobileMenuOpen ? faTimes : faBars} />
+              <FontAwesomeIcon icon={faBars} className="hamburger-icon" />
             </button>
           </div>
         </div>
       </div>
       <div
-        className={`${mobileMenuOpen ? "mobile-menu-active" : "mobile-menu"}`}
+        className={`${mobileMenuOpen ? "mobile-menu--active" : "mobile-menu"}`}
       >
-        <nav className="main-nav">
-          <ul>
-            <li>
-              <a href="/" onClick={() => setMobileMenuOpen(false)}>
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="/blog" onClick={() => setMobileMenuOpen(false)}>
-                Blog
-              </a>
-            </li>
-            <li>
-              <a
-                href="/visualizations"
-                onClick={() => setMobileMenuOpen(false)}
-              >
-                Visualizations
-              </a>
-            </li>
-          </ul>
-        </nav>
+        <div className="mobile-menu-nav">
+          <nav>
+            <ul>
+              <li>
+                <a href="/" onClick={() => setMobileMenuOpen(false)}>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="/blog" onClick={() => setMobileMenuOpen(false)}>
+                  Blog
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/visualizations"
+                  onClick={() => setMobileMenuOpen(false)}
+                >
+                  Visualizations
+                </a>
+              </li>
+              <li>
+                <a href="/contact" onClick={() => setMobileMenuOpen(false)}>
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="mobile-connect">
+            Connect{" "}
+            <span
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginLeft: "1rem",
+              }}
+            >
+              {" "}
+              <i className="arrow down"></i>
+            </span>
+          </div>
+          <div className="mobile-social-icons">
+            <a
+              href="https://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Facebook"
+            >
+              <div className="mobile-social-icon">
+                <div>Facebook</div>
+                <div className="social-icon-logo">
+                  <FontAwesomeIcon icon={faFacebook} />
+                </div>
+              </div>
+            </a>
 
-        <div className="social-icons">
-          <a
-            href="https://twitter.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Twitter"
-          >
-            <FontAwesomeIcon icon={faTwitter} />
-          </a>
-          <a
-            href="https://facebook.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Facebook"
-          >
-            <FontAwesomeIcon icon={faFacebook} />
-          </a>
-          <a
-            href="https://instagram.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Instagram"
-          >
-            <FontAwesomeIcon icon={faInstagram} />
-          </a>
-          <a
-            href="https://github.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="GitHub"
-          >
-            <FontAwesomeIcon icon={faGithub} />
-          </a>
+            <a
+              href="https://instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Instagram"
+            >
+              <div className="mobile-social-icon">
+                <div>Instagram</div>
+                <div className="social-icon-logo">
+                  <FontAwesomeIcon icon={faInstagram} />
+                </div>
+              </div>
+            </a>
+            <a
+              href="https://threads.net"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Threads"
+            >
+              <div className="mobile-social-icon">
+                <div>Threads</div>
+                <div className="social-icon-logo">
+                  <FontAwesomeIcon icon={faThreads} />
+                </div>
+              </div>
+            </a>
+
+            <a
+              href="https://x.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="xTwitter"
+            >
+              <div className="mobile-social-icon">
+                <div>X</div>
+                <div className="social-icon-logo">
+                  <FontAwesomeIcon icon={faXTwitter} />
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div className="mobile-menu-close" onClick={toggleMobileMenu}>
+          <FontAwesomeIcon icon={faTimes} size="2xl" color="black" />
         </div>
       </div>
     </div>
